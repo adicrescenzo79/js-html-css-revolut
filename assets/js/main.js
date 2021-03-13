acc2d282d9f33b12426f5f6f1b63ac5feb52d599
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-13 17:35:39 +0100

    inizio resto sito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,32 @@
-var callSub = $('header .right a.call-sub');
-
-callSub.mouseenter(function() {
-  var name = $(this).attr('name');
-  var subMenu = $('header .right .pop-up .sub-menu[name=' + name + ']');
-  subMenu.addClass('active');
-})
-
-callSub.mouseleave(function() {
-  var name = $(this).attr('name');
-  var subMenu = $('header .right .pop-up .sub-menu[name=' + name + ']');
-  subMenu.removeClass('active');
+$(document).ready(function(){
+  popUpMenu();
 })
 
 
-var subMenu = $('header .right .pop-up .sub-menu');
+// FUNCTIONS
+function popUpMenu(){
+  var callSub = $('header .right a.call-sub');
 
-subMenu.mouseenter(function() {
+  callSub.mouseenter(function() {
+    var name = $(this).attr('name');
+    var subMenu = $('header .right .pop-up .sub-menu[name=' + name + ']');
+    subMenu.addClass('active');
+  })
+
+  callSub.mouseleave(function() {
+    var name = $(this).attr('name');
+    var subMenu = $('header .right .pop-up .sub-menu[name=' + name + ']');
+    subMenu.removeClass('active');
+  })
+
+
+  var subMenu = $('header .right .pop-up .sub-menu');
+
+  subMenu.mouseenter(function() {
     $(this).addClass('active');
-})
+  })
 
-subMenu.mouseleave(function() {
+  subMenu.mouseleave(function() {
     $(this).removeClass('active');
-})
+  })
+}
